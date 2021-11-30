178f45388093816e9a87fe4b4ae26524c443c010
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2021-11-30 17:57:04 +0300

    box macı

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Fighter f1 = new Fighter("Ali", 10, 120, 100,30);
+        Fighter f2= new Fighter("Mahmut",20,85,85,40);
+
+        Match match= new Match(f1,f2,85,100);
+        match.run();
+
+    }
+}
